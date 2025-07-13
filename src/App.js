5531b8f7228a0c1f8f49c2214b236dfc8a859f95
봉재환 <bong.catholic@gmail.com>
2025-07-13 19:15:50 +0900

    Home 화면 추가 및 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import AboutCertification from "./pages/AboutCertification";
 import CheckQualification from "./pages/CheckQualification";
-import FindCenter from "./pages/FindCenter";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<h2>안식처 찾기 - 신뢰할 수 있는 심리센터를 위한 가이드</h2>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about-certification" element={<AboutCertification />} />
         <Route path="/check-qualification" element={<CheckQualification />} />
-        <Route path="/find-center" element={<FindCenter />} />
       </Routes>
     </Router>
   );
